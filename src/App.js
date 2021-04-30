e9c37a154d1d843137d472946d0c0f08df01590b
Satnesh <satneshchauhan456@gmail.com>
2021-04-30 16:33:37 +0530

    add all my files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import Text from './Text'
 
 function App() {
+  const[count,setUpdate]=useState(0);
+  const[text,setText]=useState([]);
+
+  const handleSubmit=(e)=>{
+      e.preventDefault();
+      let amount =parseInt(count);
+      if(count <=0 ){
+        amount =1
+      }if(count >8 ){
+        amount =8
+      }
+      console.log(amount)
+    //  console.log(Text)
+    setText(Text.slice(0,amount))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Paragraph generator</h1>
+     <form onSubmit={handleSubmit}>
+      <label htmlFor="amount">Paragraphs:</label>
+     <input type="number" value={count} onChange={(e)=>setUpdate(e.target.value)}></input>
+     <button>Generate</button>
+     </form>
+     <div>
+       {text.map((item,index)=>{
+         return <p key={index}>{item}</p>
+       })}
+     </div>
     </div>
   );
 }
